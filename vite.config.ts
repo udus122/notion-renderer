919facc4c645873888df3999e76ca4c022d8a301
yusuda <yusuda@yahoo-corp.jp>
2023-04-02 23:42:53 +0900

    build(vite): export type file

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { resolve } from "path";
 
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     sourcemap: true,
     lib: {
