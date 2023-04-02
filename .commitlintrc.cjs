cb492ff30b9cc6c880ed32781a69f9ac774762f9
yusuda <yusuda@yahoo-corp.jp>
2023-04-02 11:33:08 +0900

    feat: 🎸 first commit

---
+++ .commitlintrc.cjs
@@ -0,0 +1,13 @@
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci]/;
+
+const commitlintConfig = {
+  "extends": [
+    "@commitlint/config-conventional"
+  ],
+
+  ignores: [
+    commitMsg => automaticCommitPattern.test(commitMsg)
+  ]
+};
+
+module.exports = commitlintConfig;
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      name: "notion-component",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
