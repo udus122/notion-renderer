8c32287f1f27809a8d3c58780e02b6f9955cfce5
yusuda <yusuda@yahoo-corp.jp>
2023-09-27 20:56:29 +0900

    style: remove comment and add new lines

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,25 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-mdx-gfm",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
   docs: {
     autodocs: "tag",
   },
+
+  core: {
+    disableWhatsNewNotifications: true
+  }
 };
 export default config;
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,5 @@
 import type { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
-export type BlockObject = BlockObjectResponse; // | ListWrapperObject;
-
-export type BlockComponentProps<T extends BlockObject> = {
-  block?: T;
-  blocks?: BlockObjectResponse[];
-  children?: React.ReactNode;
-  // mapper?: BlockComponentMapper;
+export type BlockComponentProps<T extends BlockObjectResponse> = {
+  block: T;
 };
