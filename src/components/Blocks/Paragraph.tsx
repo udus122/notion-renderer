7399031f439d2585c31fd183d15ba0549ef0a31a
yusuda <yusuda@yahoo-corp.jp>
2023-09-28 12:43:43 +0900

    feat: enabled recursive rendering of child blocks

--- src/components/Blocks/Paragraph.tsx
+++ src/components/Blocks/Paragraph.tsx
@@ -3,22 +3,35 @@ import { clsx } from "clsx";
 import { RichTexts } from "@/components/RichTexts";
 import { generateBlockColorClass } from "@/libs";
 
+import Blocks from ".";
+
 import type { BlockComponentProps } from "@/types";
 import type { ParagraphBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import type { ListBlockChildrenResponseEx } from "notionate";
 
-type Props = BlockComponentProps<ParagraphBlockObjectResponse>;
+type Props = BlockComponentProps<
+  ParagraphBlockObjectResponse & {
+    children?: ListBlockChildrenResponseEx;
+  }
+>;
 
 const Paragraph: React.FC<Props> = ({ block }) => {
-  const blockType = "notion_paragraph";
   const blockColor = generateBlockColorClass(block.paragraph.color);
+
   return (
-    <div id={block.id} className={clsx("notion_block", blockType, blockColor)}>
+    <div
+      id={block.id}
+      className={clsx("notion_block", "notion_paragraph", blockColor)}
+    >
       <p>
         <RichTexts richText={block.paragraph.rich_text} />
       </p>
-      <div className={`${blockType}__children`}>
-        {/* TODO: 子要素を再帰的にレンダリングするようにする
-        <Blocks blocks={children}></Blocks> */}
+      <div className={"notion_children_block"}>
+        {block?.children && (
+          <div>
+            <Blocks blocks={block.children} />
+          </div>
+        )}
       </div>
     </div>
   );
