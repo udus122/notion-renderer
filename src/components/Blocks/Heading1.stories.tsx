b1a141a6140ae814a5d57390c7ab0a19cfc2ffa3
yusuda <yusuda@yahoo-corp.jp>
2023-09-28 14:58:06 +0900

    feat: create Heading1 component

---
+++ src/components/Blocks/Heading1.stories.tsx
@@ -0,0 +1,22 @@
+import { Heading1BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+import Heading1 from "./Heading1";
+import Heading1Object from "./Heading1.json";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Heading1> = {
+  title: "Blocks/Heading1",
+  component: Heading1,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Heading1>;
+
+export const Default: Story = {
+  args: {
+    block: Heading1Object as Heading1BlockObjectResponse,
+  },
+};
---
+++ src/components/Blocks/Heading1.tsx
@@ -0,0 +1,21 @@
+import { RichTexts } from "../RichTexts";
+
+import type { BlockComponentProps } from "@/types";
+import type { Heading1BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import type { ListBlockChildrenResponseEx } from "notionate";
+
+type Props = BlockComponentProps<
+  Heading1BlockObjectResponse & {
+    children?: ListBlockChildrenResponseEx;
+  }
+>;
+
+const Heading1: React.FC<Props> = ({ block }) => {
+  return (
+    <h1 id={block.id} className="notion_block notion_heading notion_heading_1">
+      <RichTexts richText={block.heading_1.rich_text} />
+    </h1>
+  );
+};
+
+export default Heading1;
