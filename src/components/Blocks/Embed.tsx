34dabb2ea31e22c6544e8b6a13c8c9f7f3eca7d2
yusuda <yusuda@yahoo-corp.jp>
2023-10-07 15:48:15 +0900

    feat: embed component can deal with Twitter(X) link

--- src/components/Blocks/Embed.tsx
+++ src/components/Blocks/Embed.tsx
@@ -1,3 +1,5 @@
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
 import RichTexts from "../RichTexts/RichTexts";
 
 import type { BlockComponentProps, EmbedBlockObjectComponent } from "@/types";
@@ -5,6 +7,11 @@ import type { BlockComponentProps, EmbedBlockObjectComponent } from "@/types";
 type Props = BlockComponentProps<EmbedBlockObjectComponent>;
 
 const Embed: React.FC<Props> = ({ block }) => {
+  const tweetId = getTweetId(block.embed.url);
+  if (tweetId) {
+    return <TwitterTweetEmbed options={{ theme: "dark" }} tweetId={tweetId} />;
+  }
+
   return (
     <div id={block.id} className="notion_embed">
       <iframe src={block.embed.url} />
@@ -16,3 +23,47 @@ const Embed: React.FC<Props> = ({ block }) => {
 };
 
 export default Embed;
+
+/* https://qiita.com/ATTACK3500/items/091bedc2bf0596e08f17 */
+const getTweetId = (url: string) => {
+  if (!isTweetUrl(url)) return null;
+
+  const regexes = [
+    /https?:\/\/twitter\.com\/(\w+)\/status(es)?\/(\d+)/,
+    /https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/,
+    /https?:\/\/mobile\.twitter\.com\/(\w+)\/status(es)?\/(\d+)/,
+  ];
+
+  for (const regex of regexes) {
+    const match = url.match(regex);
+    if (match) {
+      return match[match.length - 1];
+    }
+  }
+
+  return null;
+};
+
+const isTweetUrl = (url: string) => {
+  if (!isTwitterUrl(url)) return false;
+  const regex =
+    /^https?:\/\/(?:www\.|mobile\.)?twitter\.com\/(?:#!\/)?([a-zA-Z0-9_]{1,15})\/status(?:es)?\/(\d+)(?:\?.*)?$/;
+  return regex.test(url);
+};
+
+const isTwitterUrl = (url: string) => {
+  const twitterHostnames = [
+    "twitter.com",
+    "www.twitter.com",
+    "mobile.twitter.com",
+    // 'pbs.twimg.com',
+    // 't.co',
+  ];
+
+  try {
+    const parser = new URL(url);
+    return twitterHostnames.includes(parser.hostname);
+  } catch (error) {
+    return false;
+  }
+};
