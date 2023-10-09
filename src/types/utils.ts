f3d93ce09f810d60974795adf706f016142c6c19
yusuda <yusuda@yahoo-corp.jp>
2023-10-09 17:03:00 +0900

    chore: create Result Utility type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -3,3 +3,13 @@ export type Overwrite<T, U extends { [Key in keyof T]?: unknown }> = Omit<
   keyof U
 > &
   U;
+
+export type Result<T, E> =
+  | {
+      payload: T;
+      error?: undefined;
+    }
+  | {
+      payload?: undefined;
+      error: E;
+    };
