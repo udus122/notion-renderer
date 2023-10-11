5576d5f319a10a62b2687d1368041f311b489b7f
yusuda <yusuda@yahoo-corp.jp>
2023-10-11 17:58:26 +0900

    feat: create LinkToPage block

---
+++ src/components/Blocks/LinkToPage.stories.tsx
@@ -0,0 +1,21 @@
+import LinkToPage from "./LinkToPage";
+import LinkToPageObject from "./LinkToPage.json";
+
+import type { LinkToPageBlockObjectComponent } from "@/types/components";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof LinkToPage> = {
+  title: "Blocks/LinkToPage",
+  component: LinkToPage,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LinkToPage>;
+
+export const Default: Story = {
+  args: {
+    block: LinkToPageObject as LinkToPageBlockObjectComponent,
+  },
+};
---
+++ src/components/Blocks/LinkToPage.tsx
@@ -0,0 +1,56 @@
+import { extractTitleProperty } from "@/libs/utils";
+
+import RichTexts from "../RichTexts/RichTexts";
+
+import Icon from "./Icon";
+
+import type {
+  BlockComponentProps,
+  LinkToPageBlockObjectComponent,
+} from "@/types/components";
+
+type Props = BlockComponentProps<LinkToPageBlockObjectComponent>;
+
+const LinkToPage: React.FC<Props> = ({ block }) => {
+  if (block.link_to_page.type === "page_id" && block.link_to_page.page) {
+    const titleProperty = extractTitleProperty(block.link_to_page.page);
+    return (
+      <a href={`/${block.id}`}>
+        <div id={block.id} className="notion_link_to_page">
+          <span className="notion_link_to_page_icon">
+            {<Icon icon={block.link_to_page.page?.icon ?? null} />}
+          </span>
+          <span className="notion_link_to_page_title">
+            <RichTexts richTexts={titleProperty.title} />
+          </span>
+        </div>
+      </a>
+    );
+  }
+  if (
+    block.link_to_page.type === "database_id" &&
+    block.link_to_page.database
+  ) {
+    const titleProperty = extractTitleProperty(block.link_to_page.database);
+
+    return (
+      <a href={`/${block.id}`}>
+        <div id={block.id} className="notion_link_to_page">
+          <span className="notion_link_to_page_icon">
+            {<Icon icon={block.link_to_page.database?.icon ?? null} />}
+          </span>
+          <span className="notion_link_to_page_title">
+            <RichTexts richTexts={titleProperty.title} />
+          </span>
+        </div>
+      </a>
+    );
+  }
+  if (block.link_to_page.type === "comment_id" && block.link_to_page.comments) {
+    console.warn("comment type link_to_page block is not supported yet.");
+  }
+
+  return null;
+};
+
+export default LinkToPage;
