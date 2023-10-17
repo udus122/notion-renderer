0ba69b1f9779c66c035e788f34475c0066572948
yusuda <yusuda@yahoo-corp.jp>
2023-10-17 15:40:30 +0900

    chore: update storybook packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,15 @@ import { resolve } from "path";
 
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react()],
   build: {
     sourcemap: true,
     lib: {
+      name: "NotionComponents",
       entry: resolve(__dirname, "src/index.ts"),
-      fileName: "index",
-      name: "notion-component",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
