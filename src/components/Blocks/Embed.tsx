85b065bad4359eabfb3b46fe091e9909e0b7ad19
yusuda <yusuda@yahoo-corp.jp>
2023-10-21 11:31:54 +0900

    chore: remove 3rd party library

--- src/components/Blocks/Embed.tsx
+++ src/components/Blocks/Embed.tsx
@@ -8,11 +8,17 @@ import type {
 type Props = BlockComponentProps<EmbedBlockObjectComponent>;
 
 export const Embed: React.FC<Props> = ({ block }) => {
-  const tweetId = getTweetId(block.embed.url);
-  if (tweetId) {
-    // return <TwitterTweetEmbed options={{ theme: "dark" }} tweetId={tweetId} />;
-    // TODO: eEmbed APIによる描画に変更する
-    return null;
+  if (block.embed.oembed) {
+    return (
+      <div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html:
+              block.embed.oembed.type === "rich" ? block.embed.oembed.html : "",
+          }}
+        />
+      </div>
+    );
   }
 
   return (
@@ -23,48 +29,4 @@ export const Embed: React.FC<Props> = ({ block }) => {
       </div>
     </div>
   );
-};
-
-/* https://qiita.com/ATTACK3500/items/091bedc2bf0596e08f17 */
-const getTweetId = (url: string) => {
-  if (!isTweetUrl(url)) return null;
-
-  const regexes = [
-    /https?:\/\/twitter\.com\/(\w+)\/status(es)?\/(\d+)/,
-    /https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/,
-    /https?:\/\/mobile\.twitter\.com\/(\w+)\/status(es)?\/(\d+)/,
-  ];
-
-  for (const regex of regexes) {
-    const match = url.match(regex);
-    if (match) {
-      return match[match.length - 1];
-    }
-  }
-
-  return null;
-};
-
-const isTweetUrl = (url: string) => {
-  if (!isTwitterUrl(url)) return false;
-  const regex =
-    /^https?:\/\/(?:www\.|mobile\.)?twitter\.com\/(?:#!\/)?([a-zA-Z0-9_]{1,15})\/status(?:es)?\/(\d+)(?:\?.*)?$/;
-  return regex.test(url);
-};
-
-const isTwitterUrl = (url: string) => {
-  const twitterHostnames = [
-    "twitter.com",
-    "www.twitter.com",
-    "mobile.twitter.com",
-    // 'pbs.twimg.com',
-    // 't.co',
-  ];
-
-  try {
-    const parser = new URL(url);
-    return twitterHostnames.includes(parser.hostname);
-  } catch (error) {
-    return false;
-  }
 };
--- src/components/Blocks/Video.tsx
+++ src/components/Blocks/Video.tsx
@@ -1,5 +1,3 @@
-// import YouTube from "react-youtube";
-
 import { RichTexts } from "../RichTexts/index.js";
 
 import type {
@@ -17,18 +15,21 @@ export const Video: React.FC<Props> = ({ block }) => {
       ? block.video.file.url
       : "";
 
-  const youTubeId = getYouTubeId(videoUrl);
-
-  if (youTubeId) {
-    //    // @ts-expect-error: Error caused by being treated as a commonjs library because the library's package.json does not have "type": "module"
-    // return <YouTube videoId={youTubeId} />;
-    return null;
+  if (block.video.oembed) {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html:
+            block.video.oembed.type === "video" ? block.video.oembed.html : "",
+        }}
+      />
+    );
   }
 
   return (
     <div id={block.id} className="notion_video">
       <video controls src={videoUrl}>
-        Your browser does not support HTML5 videos. You can download video file
+        Your browser does not support type os. You can download video file
         <a href={videoUrl}>here</a>.
       </video>
       <div className="notion_caption notion_video_caption">
@@ -36,13 +37,4 @@ export const Video: React.FC<Props> = ({ block }) => {
       </div>
     </div>
   );
-};
-
-const getYouTubeId = (url: string) => {
-  const arr = url.split(/(vi\/|v%3D|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-  if (arr[0] === url) {
-    // if cannot find YouTubeId, return undefined.
-    return undefined;
-  }
-  return undefined !== arr[2] ? arr[2].split(/[^\w-]/i)[0] : arr[0];
 };
