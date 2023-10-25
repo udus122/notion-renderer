35e2bbcce2f20094a233e971ceeaa20ca5f1f4de
udus122 <u.yudai1202@gmail.com>
2023-10-25 03:41:42 +0000

    Deploying to gh-pages from @ udus122/notion-components@757caa29555ff0936edf552452d9789b66a92736 🚀

---
+++ assets/Annotation.stories-62bdcaf8.js
@@ -0,0 +1,7 @@
+import{A as r}from"./Text-73a49d78.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Link-b5909185.js";import"./Divider-788acd01.js";import"./Equation-a7b58347.js";import"./index-a34f34ea.js";import"./Bold-e93c1dbf.js";import"./Color-01c1a558.js";import"./InlineCode-a40eec31.js";import"./Italic-ab7b1ad2.js";import"./Strikethrough-950f48a2.js";import"./Underline-504dd5a5.js";const i="text",a={content:"rich text with all",link:{url:"https://example.com"}},c={bold:!0,italic:!0,strikethrough:!0,underline:!0,code:!0,color:"brown"},s="rich text with all",m=null,p={type:i,text:a,annotations:c,plain_text:s,href:m},R={title:"Annotation",component:r,tags:["autodocs"]},t={args:{richTextItem:p,children:"text with all annotation"}};var o,e,n;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    richTextItem: (RichTextAnnotationObject as RichTextItem),
+    children: "text with all annotation"
+  }
+}`,...(n=(e=t.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const j=["Default"];export{t as Default,j as __namedExportsOrder,R as default};
+//# sourceMappingURL=Annotation.stories-62bdcaf8.js.map
--- assets/Annotation.stories-fa820c75.js
+++
@@ -1,7 +0,0 @@
-import{A as r}from"./Text-c12badd7.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Bold-e93c1dbf.js";import"./Color-01c1a558.js";import"./InlineCode-a40eec31.js";import"./Italic-ab7b1ad2.js";import"./Strikethrough-950f48a2.js";import"./Underline-504dd5a5.js";import"./Link-b5909185.js";import"./Equation-a7b58347.js";import"./index-a34f34ea.js";import"./Divider-788acd01.js";const i="text",a={content:"rich text with all",link:{url:"https://example.com"}},c={bold:!0,italic:!0,strikethrough:!0,underline:!0,code:!0,color:"brown"},s="rich text with all",m=null,p={type:i,text:a,annotations:c,plain_text:s,href:m},R={title:"Annotation",component:r,tags:["autodocs"]},t={args:{richTextItem:p,children:"text with all annotation"}};var o,e,n;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    richTextItem: (RichTextAnnotationObject as RichTextItem),
-    children: "text with all annotation"
-  }
-}`,...(n=(e=t.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const j=["Default"];export{t as Default,j as __namedExportsOrder,R as default};
-//# sourceMappingURL=Annotation.stories-fa820c75.js.map
