1c80b362d4ef01ac70872e564e776c9a56c5034c
yusuda <yusuda@yahoo-corp.jp>
2023-10-31 17:57:42 +0900

    feat: create Title component

---
+++ src/components/Title.stories.tsx
@@ -0,0 +1,21 @@
+import { Title } from "./Title.js";
+import TitleObject from "./Title.json";
+
+import type { Meta, StoryObj } from "@storybook/react";
+import type { RichText } from "src/types/notion/richText/richText.js";
+
+const meta: Meta<typeof Title> = {
+  title: "Title",
+  component: Title,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Title>;
+
+export const Default: Story = {
+  args: {
+    title: TitleObject as RichText,
+  },
+};
