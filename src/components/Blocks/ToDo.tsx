7e8e5224a4c97a4fc5f5f847ec88460c040e8d09
yusuda <yusuda@yahoo-corp.jp>
2023-11-01 11:21:21 +0900

    refactor: make Checkbox common component

--- src/components/Blocks/ToDo.tsx
+++ src/components/Blocks/ToDo.tsx
@@ -1,3 +1,4 @@
+import { Checkbox } from "../Checkbox.js";
 import { RichText } from "../RichText/RichText.js";
 
 import { BlockList } from "./BlockList.js";
@@ -5,7 +6,8 @@ import { BlockList } from "./BlockList.js";
 import type { ToDoBlock } from "../../types/notion/blocks/toDo.js";
 
 export const ToDo: ToDoBlock = ({ block }) => {
-  const isChecked = block.to_do.checked;
+  const { checked } = block.to_do;
+
   return (
     <div
       id={block.id}
@@ -13,9 +15,9 @@ export const ToDo: ToDoBlock = ({ block }) => {
     >
       <div className="notion-to-do-content">
         <span className="notion-to-do-icon">
-          {isChecked ? <CheckedBox /> : <CheckBox />}
+          <Checkbox checked={checked} />
         </span>
-        <span className={`notion-to-do-text ${isChecked ? "checked" : ""}`}>
+        <span className={`notion-to-do-text ${checked ? "checked" : ""}`}>
           <RichText richText={block.to_do.rich_text} />
         </span>
       </div>
@@ -27,19 +29,3 @@ export const ToDo: ToDoBlock = ({ block }) => {
     </div>
   );
 };
-
-const CheckBox = () => (
-  <span className="notion_checkbox">
-    <svg viewBox="0 0 16 16">
-      <path d="M1.5,1.5 L1.5,14.5 L14.5,14.5 L14.5,1.5 L1.5,1.5 Z M0,0 L16,0 L16,16 L0,16 L0,0 Z"></path>
-    </svg>
-  </span>
-);
-
-const CheckedBox = () => (
-  <span className="notion-checkbox checked">
-    <svg viewBox="0 0 14 14">
-      <polygon points="5.5 11.9993304 14 3.49933039 12.5 2 5.5 8.99933039 1.5 4.9968652 0 6.49933039"></polygon>
-    </svg>
-  </span>
-);
