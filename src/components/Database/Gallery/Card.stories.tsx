0a569382cd4627c60c4a59b2d4da950a872b22ff
yusuda <yusuda@yahoo-corp.jp>
2023-11-13 16:08:52 +0900

    fix: add notion- prefix to class

--- src/components/Database/Gallery/Card.stories.tsx
+++ src/components/Database/Gallery/Card.stories.tsx
@@ -5,7 +5,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import type { PageObject } from "src/types/notion/pages/page.js";
 
 const meta: Meta<typeof Card> = {
-  title: "Database/Gallery/Card",
+  title: "Database/GalleryCard",
   component: Card,
   tags: ["autodocs"],
   parameters: {
--- src/components/Database/Gallery/Card.tsx
+++ src/components/Database/Gallery/Card.tsx
@@ -1,7 +1,7 @@
 import { splitTitleAndOtherProperties } from "../../../utils.js";
+import { Cover } from "../../Common/Cover.js";
 import { Icon } from "../../Common/Icon.js";
 import { useMapper } from "../../hooks.js";
-import { Cover } from "../../Common/Cover.js";
 import { Properties } from "../../Property/Properties.js";
 
 import type { PageObject } from "../../../types/notion/pages/page.js";
@@ -14,19 +14,19 @@ export const Card = ({ page }: { page: PageObject }) => {
   return (
     <div id={page.id} className="notion-gallery-card">
       <Link prefix="/" link={page.id}>
-        <div className="gallery-card-cover">
+        <div className="notion-gallery-card-cover">
           <Cover cover={page.cover} />
         </div>
-        <div className="gallery-card-title">
+        <div className="notion-gallery-card-title">
           <span className="notion-gallery-card-title-icon">
-            <Icon icon={page.icon} />
+            <Icon icon={page.icon} hideDefaultIcon />
           </span>
           <span className="notion-gallery-card-title-text">
             {title?.title.map((text) => text.plain_text).join("") ??
               "Annoymous"}
           </span>
         </div>
-        <div className="gallery-card-properties">
+        <div className="notion-gallery-card-properties">
           <Properties properties={other} hidePropertyName />
         </div>
       </Link>
