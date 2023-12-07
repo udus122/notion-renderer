d24feac982e40999f7e20eee53d28bcb045aba88
udus122 <u.yudai1202@gmail.com>
2023-12-07 10:07:03 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@ac74e06a2903c93a317198b8865607728dcebf31 🚀

---
+++ assets/Page.stories-5f2a0bce.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{P as d,a as c}from"./Page-cde2eebb.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./utils-957ad31c.js";import"./Cover-4eaf6d1c.js";import"./Icon-969464dd.js";import"./Title-e59e0953.js";import"./Annotation-f06e66d1.js";import"./hooks-122813d9.js";import"./Link-0787c4a7.js";import"./AnnotationItem-8136528e.js";import"./Bold-27d453fc.js";import"./Color-bf0161c1.js";import"./InlineCode-aab75cea.js";import"./Italic-ec5ae852.js";import"./Strikethrough-2d45501b.js";import"./Underline-53eade56.js";import"./RichText-3472ed79.js";import"./RichTextItem-12a01c82.js";import"./InlineEquation-b4548cae.js";import"./katex-c2839aef.js";import"./Mention-4e938830.js";import"./index-077c9703.js";import"./index-7653c3a3.js";import"./Date-8aa40b7c.js";import"./Text-0d32d05a.js";import"./BlockList-fd0bb96f.js";import"./Audio-be941693.js";import"./Bookmark-e4c3f15a.js";import"./Breadcrumb-bf91866d.js";import"./ChildDatabase-efcc6198.js";import"./ChildPage-ee689288.js";import"./Code-18f0f11e.js";import"./Divider-8ddfd6ee.js";import"./Embed-ff8c0653.js";import"./Equation-a42dba42.js";import"./File-7d951419.js";import"./File-f7d35f8a.js";import"./Image-279f2e9a.js";import"./LinkPreview-e679f58f.js";import"./LinkToPage-78c08d5f.js";import"./Pdf-440e317e.js";import"./Table-8b00dd5a.js";import"./TableOfContents-cd614a99.js";import"./Checkbox-c6fdcc69.js";import"./Video-1e2201a4.js";import"./Renderer-c329bb42.js";import"./Properties-ecbbb5fb.js";import"./Checkbox-3be99024.js";import"./CreatedBy-c583fb03.js";import"./CreatedTime-9e0b4a9c.js";import"./index-c4405eea.js";import"./Date-27380008.js";import"./Email-f4f504c8.js";import"./Files-dcaeb88f.js";import"./Formula-db617389.js";import"./LastEditedBy-6a375088.js";import"./LastEditedTime-f80e44d7.js";import"./MultiSelect-05b0bdc6.js";import"./Number-6f1a1711.js";import"./People-93a32012.js";import"./PhoneNumber-96b91d09.js";import"./Relation-c04cae31.js";import"./RichText-7618085a.js";import"./Rollup-168f406f.js";import"./Select-a00880f0.js";import"./Status-44ce3bab.js";import"./Title-5b6008a7.js";import"./UniqueId-7bf511e5.js";import"./Url-a7077e9a.js";const Rt={title:"Page",component:d,tags:["autodocs"]},t={args:{page:c},decorators:[i=>r.jsx("div",{className:"notion-root notion-light",children:r.jsx(i,{})})]},o={args:{page:c,displayProperties:["USxG","Tags","Text","Multi-select","Status","Last edited time","Related Child database","Select","Date","Person","Number","Checkbox","URL","Email","Phone"]},decorators:[i=>r.jsx("div",{style:{blockSize:"100vh"},className:"notion-root notion-dark",children:r.jsx(i,{})})]};var e,m,p;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    page: (PageObject as PageObjectType)
+  },
+  decorators: [Story => <div className="notion-root notion-light">
+        <Story />
+      </div>]
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var a,s,n;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    page: (PageObject as PageObjectType),
+    displayProperties: ["USxG", "Tags", "Text", "Multi-select", "Status", "Last edited time", "Related Child database", "Select", "Date", "Person", "Number", "Checkbox", "URL", "Email", "Phone"]
+  },
+  decorators: [Story => <div style={{
+    blockSize: "100vh"
+  }} className="notion-root notion-dark">
+        <Story />
+      </div>]
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const Ct=["Default","Dark"];export{o as Dark,t as Default,Ct as __namedExportsOrder,Rt as default};
+//# sourceMappingURL=Page.stories-5f2a0bce.js.map
