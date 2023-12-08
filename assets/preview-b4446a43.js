17c3f6336089d1f6d425bcf383ea8ba8e3057b80
udus122 <u.yudai1202@gmail.com>
2023-12-08 06:12:47 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@1d556314bf4fdab954fe1ab53063702424d0696b 🚀

--- assets/preview-b4446a43.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},layout:"fullscreen"}};export{e as default};
-//# sourceMappingURL=preview-b4446a43.js.map
