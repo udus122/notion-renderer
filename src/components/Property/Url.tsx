df9922858c0c5216896e78095423aa89ed6a99da
yusuda <yusuda@yahoo-corp.jp>
2023-12-09 12:17:49 +0900

    fix: remove .notion-link from url component

--- src/components/Property/Url.tsx
+++ src/components/Property/Url.tsx
@@ -3,9 +3,7 @@ import type { UrlPropertyItemComponent } from "../../types/notion/pages/properti
 export const UrlProperty: UrlPropertyItemComponent = ({ propertyItem }) => {
   return (
     <div id={propertyItem.id} className="notion-property-item notion-url">
-      <a className="notion-link" href={propertyItem.url ?? ""}>
-        {propertyItem.url}
-      </a>
+      <a href={propertyItem.url ?? ""}>{propertyItem.url}</a>
     </div>
   );
 };
