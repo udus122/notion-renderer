4435e2af4577b588671e9c5df7dff65100d88f88
yusuda <yusuda@yahoo-corp.jp>
2023-12-09 12:11:45 +0900

    fix: file components

--- src/components/Blocks/File.tsx
+++ src/components/Blocks/File.tsx
@@ -1,24 +1,11 @@
 import { File as FileComponent } from "../Common/File.js";
-import { useMapper } from "../hooks.js";
 import { RichText } from "../RichText/RichText.js";
 
 import type { FileBlock } from "../../types/notion/blocks/file.js";
 
 export const File: FileBlock = ({ block }) => {
-  const { Link } = useMapper();
-
-  const fileUrl =
-    block.file.type == "external"
-      ? block.file.external.url
-      : block.file.type == "file"
-      ? block.file.file.url
-      : "";
-  const filename = decodeURIComponent(
-    new URL(fileUrl).pathname.split("/").slice(-1)[0]
-  );
-
   return (
-    <div id={block.id} className="notion-file">
+    <div id={block.id} className="notion-file-block">
       <FileComponent file={block.file} />
       <div className="notion-caption notion-file-caption">
         <RichText richText={block.file.caption} />
