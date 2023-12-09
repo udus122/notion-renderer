e440d92303eee81e584ac1a17348a7d3b540136e
yusuda <yusuda@yahoo-corp.jp>
2023-12-09 12:14:33 +0900

    fix: remove .notion-link from email

--- src/components/Property/Email.tsx
+++ src/components/Property/Email.tsx
@@ -4,9 +4,7 @@ export const EmailProperty: EmailPropertyItemComponent = ({ propertyItem }) => {
   return (
     <div id={propertyItem.id} className="notion-property-item notion-email">
       {propertyItem.email ? (
-        <a className="notion-link" href={`mailto:${propertyItem.email}`}>
-          {propertyItem.email}
-        </a>
+        <a href={`mailto:${propertyItem.email}`}>{propertyItem.email}</a>
       ) : (
         ""
       )}
