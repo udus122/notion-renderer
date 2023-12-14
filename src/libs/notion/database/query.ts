f3174f5cb9abc05033b126eb302fc9bbd63a7116
yusuda <yusuda@yahoo-corp.jp>
2023-12-14 11:19:02 +0900

    refactor: move fetchAllDatabaseItems to query

--- src/libs/notion/database/query.ts
+++ src/libs/notion/database/query.ts
@@ -6,6 +6,7 @@ import type {
   QueryDatabaseParameters,
   QueryDatabaseResponse,
 } from "@notionhq/client/build/src/api-endpoints.js";
+import type { PageObject } from "src/types/notion/pages/page.js";
 
 export const queryDatabase = async (
   args: QueryDatabaseParameters,
@@ -16,4 +17,26 @@ export const queryDatabase = async (
   >(notion.databases.query, args);
 
   return result;
+};
+
+export const queryAllDatabaseItems = async (
+  args: QueryDatabaseParameters,
+): Promise<Array<PageObject>> => {
+  const { ok, data } = await queryDatabase(args);
+  if (!ok) {
+    return [];
+  }
+
+  // TODO: convertResponseToPageを使って型変換する
+  const results = data.results as Array<PageObject>;
+
+  if (!data.next_cursor) {
+    return results;
+  }
+
+  const nextResults = await queryAllDatabaseItems({
+    ...args,
+    start_cursor: data.next_cursor,
+  });
+  return [...results, ...nextResults];
 };
