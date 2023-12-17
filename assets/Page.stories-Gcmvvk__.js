3d85059670415ee4c8dec8cf819ca7c76760faa4
udus122 <u.yudai1202@gmail.com>
2023-12-17 01:30:02 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@f85d0dd9e9530af09358c4947d49be897aff7606 🚀

---
+++ assets/Page.stories-Gcmvvk__.js
@@ -0,0 +1,28 @@
+import{B as s}from"./BlockList-Avan3V__.js";import{O as d,a as c,b as l,c as g}from"./OpenedToggle-Ov11q1Jl.js";import{P as h}from"./Audio-VpjU8JU9.js";import{P as n}from"./PageMeta-7FbKLtUp.js";import"./jsx-runtime-vNq4Oc-g.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Annotation-fR2yJeRa.js";import"./hooks-3JhRioXY.js";import"./Link-4y-dmM-M.js";import"./AnnotationItem-s6kmnr_Q.js";import"./Bold-lS9YMGOC.js";import"./Color-mptN1qya.js";import"./InlineCode-TPhS41qP.js";import"./Italic-ORC3lBYR.js";import"./Strikethrough-g_2WBDxm.js";import"./Underline-K-lIgRJf.js";import"./InlineEquation-wvDc40t4.js";import"./katex-mFqOzwKf.js";import"./index-jnESftpm.js";import"./index-Ou0R2y6e.js";import"./Date-Pu-r1Rem.js";import"./Text-nGQm-V4-.js";import"./Icon-e4pLob1-.js";import"./ChildDatabase-SDaNFEpP.js";import"./ChildPage-lVq8yHN5.js";import"./Divider-uligtz63.js";import"./Equation-yeHPBoRl.js";import"./Files-L4SlQa9H.js";import"./LinkPreview-gmflsAnQ.js";import"./TableOfContents-p8al1xEY.js";import"./Checkbox-tcHiP4BQ.js";import"./Cover-z89RitXa.js";import"./Title-Hc14J7Em.js";import"./Checkbox-JL70vLAJ.js";import"./CreatedBy-iOJDkciQ.js";import"./CreatedTime-VvaTSUef.js";import"./index-svRVa2Vs.js";import"./Date-sXOTSNaq.js";import"./Email-dwSEzcgk.js";import"./Formula-yLIBVUXb.js";import"./LastEditedBy-O9svYQ5V.js";import"./LastEditedTime-0qC6POEY.js";import"./MultiSelect-i5MF47Mt.js";import"./Number-ngrvBzjk.js";import"./People-DfB2XZVQ.js";import"./PhoneNumber-EJEYVhPk.js";import"./Rollup-YqiuD841.js";import"./Select-5o4j6stF.js";import"./Status-7q_XHn2S.js";import"./UniqueId-zpaUPJvo.js";import"./Url-xp6E5wKB.js";const ge={title:"Page",component:h,tags:["autodocs"]},e={args:{page:n,blocks:s,displayProperties:["Status","Multi-select","Text","Tags","Created time","Last edited time","Related Child database","Select","Date","Person","Number","Checkbox","URL","Email","Phone"]},parameters:{controls:{include:["viewType","hideCover","hideIcon","hideTitle","hideProperties","theme"]}}},t={args:{page:n,blocks:s,blockMapper:{toggle:d,heading_1:c,heading_2:l,heading_3:g}},parameters:{controls:{include:["viewType","hideCover","hideIcon","hideTitle","hideDescription","theme"]}}};var o,r,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    page: (PageMetaObjectData as PageObject),
+    blocks: (BlocksObject as Array<BlockBlockObject>),
+    displayProperties: ["Status", "Multi-select", "Text", "Tags", "Created time", "Last edited time", "Related Child database", "Select", "Date", "Person", "Number", "Checkbox", "URL", "Email", "Phone"]
+  },
+  parameters: {
+    controls: {
+      include: ["viewType", "hideCover", "hideIcon", "hideTitle", "hideProperties", "theme"]
+    }
+  }
+}`,...(i=(r=e.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};var a,p,m;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    page: (PageMetaObjectData as PageObject),
+    blocks: (BlocksObject as Array<BlockBlockObject>),
+    blockMapper: {
+      toggle: OpenedToggle,
+      heading_1: OpenedHeading1,
+      heading_2: OpenedHeading2,
+      heading_3: OpenedHeading3
+    }
+  },
+  parameters: {
+    controls: {
+      include: ["viewType", "hideCover", "hideIcon", "hideTitle", "hideDescription", "theme"]
+    }
+  }
+}`,...(m=(p=t.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const he=["Default","Custom"];export{t as Custom,e as Default,he as __namedExportsOrder,ge as default};
