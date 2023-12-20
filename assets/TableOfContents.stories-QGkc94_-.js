27bbcf6c30eb73aa2260fa16a7b27c45b3232ae9
udus122 <u.yudai1202@gmail.com>
2023-12-20 04:57:01 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@a0dad521b21faf17b5966dcc6c3f739512e1fb33 🚀

---
+++ assets/TableOfContents.stories-QGkc94_-.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{B as r}from"./hooks-3JhRioXY.js";import{B as n}from"./BlockList-Avan3V__.js";import{T as d}from"./TableOfContents-p8al1xEY.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Link-4y-dmM-M.js";const l="block",i="caab8e83-7d35-466f-9138-f9156f17577d",b={type:"page_id",page_id:"2712e341-754a-41ae-a9ce-4c0bb4b18c52"},_="2023-03-18T12:21:00.000Z",f="2023-03-18T12:21:00.000Z",m={object:"user",id:"e0446ede-dde6-457c-b535-d2b19f40b45a"},p={object:"user",id:"e0446ede-dde6-457c-b535-d2b19f40b45a"},u=!1,j=!1,k="table_of_contents",B={color:"default"},y={object:l,id:i,parent:b,created_time:_,last_edited_time:f,created_by:m,last_edited_by:p,has_children:u,archived:j,type:k,table_of_contents:B},P={title:"Blocks/TableOfContents",component:d,tags:["autodocs"]},e={args:{block:y},decorators:[c=>t.jsx(r.Provider,{value:n,children:t.jsx(c,{})})]};var o,s,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    block: (TableOfContentsObject as TableOfContentsBlockObject)
+  },
+  decorators: [Story => <BlocksContext.Provider value={(BlockList as Array<BlockBlockObject>)}>
+        <Story />
+      </BlocksContext.Provider>]
+}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,P as default};
