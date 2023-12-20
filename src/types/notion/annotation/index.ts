5ccd9c6088ee607da17b6f823ce5e87b4734b966
yusuda <yusuda@yahoo-corp.jp>
2023-12-20 12:37:03 +0900

    feat: export types

---
+++ src/types/notion/annotation/index.ts
@@ -0,0 +1,6 @@
+export * from "./bold.js";
+export * from "./code.js";
+export * from "./color.js";
+export * from "./italic.js";
+export * from "./strikethrough.js";
+export * from "./underline.js";
---
+++ src/types/notion/mapper/index.ts
@@ -0,0 +1,4 @@
+export * from "./annotationItem.js";
+export * from "./block.js";
+export * from "./propertyItem.js";
+export * from "./richTextItem.js";
