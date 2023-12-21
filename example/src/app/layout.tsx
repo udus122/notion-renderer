a83267ecac235c5e6fa7871c45d21b8f19cbe209
yusuda <yusuda@yahoo-corp.jp>
2023-12-21 19:06:40 +0900

    chore: corresponding to version alpha.58

--- example/src/app/layout.tsx
+++ example/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="notion-root notion-dark">{children}</body>
     </html>
   );
 }
