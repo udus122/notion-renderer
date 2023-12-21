453ebc6ad15e9c6a9dd9260ab323ba827b0c5bf4
udus122 <u.yudai1202@gmail.com>
2023-12-21 10:03:42 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@f12bc747ef22986b840479851351d5110efcb5ce 🚀

---
+++ assets/preview-kyV5nbKt.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-jUKSV2gM.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-gf7k0r6Y.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-gf7k0r6Y.js","./iframe-jUKSV2gM.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-3kA2c_vY.js","./index-Ou0R2y6e.js","./index-ogXoivrg.js","./index-Eng-400s.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
