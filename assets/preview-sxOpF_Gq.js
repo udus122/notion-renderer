f1fd7f3ee1437e68e4b7dba2a8bc7b5d31a8100e
udus122 <u.yudai1202@gmail.com>
2023-12-25 08:56:06 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@8d7f1f500d5aa06a781559fd3dd3c8507438b76d 🚀

--- assets/preview-sxOpF_Gq.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-ldnqdnO_.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-7blLWBOa.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-7blLWBOa.js","./iframe-ldnqdnO_.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-bosEaBNP.js","./index-Ou0R2y6e.js","./index-ogXoivrg.js","./index-Eng-400s.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
