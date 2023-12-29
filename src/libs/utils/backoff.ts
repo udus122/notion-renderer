8e2cf2e54bb00e1feeab775342e3ba99d01c4563
yusuda <yusuda@yahoo-corp.jp>
2023-12-29 12:25:32 +0900

    feat: add log info

--- src/libs/utils/backoff.ts
+++ src/libs/utils/backoff.ts
@@ -6,6 +6,7 @@ export const exponentialBackoffFactory = (
   let currentDelay: number = initialDelay;
 
   return async (): Promise<void> => {
+    console.info(`Waiting ${currentDelay} seconds...`);
     await new Promise<void>((resolve) =>
       setTimeout(resolve, currentDelay * 1000),
     );
