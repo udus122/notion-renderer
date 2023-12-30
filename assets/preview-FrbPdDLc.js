1658f9f796b5fcad89b17c9ffa3b588aec4bffc0
udus122 <u.yudai1202@gmail.com>
2023-12-30 04:49:31 +0000

    Deploying to gh-pages from @ udus122/notion-renderer@abad8cf2e582df9af499c14819f3ee44415dbc02 🚀

---
+++ assets/preview-FrbPdDLc.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-1Zxk8MrS.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-B_nhDizp.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-B_nhDizp.js","./iframe-1Zxk8MrS.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-9IUWzouH.js","./index-Ou0R2y6e.js","./index-ogXoivrg.js","./index-Eng-400s.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
